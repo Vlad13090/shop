69ab1cc11220300ac247566b905d46951b2b974c
Vlad <vl_mrgn@icloud.com>
2024-08-09 03:41:54 +0300

    Обновил поиск на сайте

--- main/utils.py
+++ main/utils.py
@@ -4,14 +4,16 @@ from main.models import Shoes
 
 
 def g_search(query):
-    if len(query.split()) == 1:
-        keywords = query.split()
-    else:
-        keywords = [word for word in query.split() if len(word) > 2]
+    return Shoes.objects.filter(description__search=query)
 
-    q_obj = Q()
-    for token in keywords:
-        q_obj |= Q(name__icontains=token)
-        q_obj |= Q(description__icontains=token)
-
-    return Shoes.objects.filter(q_obj)
+    # if len(query.split()) == 1:
+    #     keywords = query.split()
+    # else:
+    #     keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_obj = Q()
+    # for token in keywords:
+    #     q_obj |= Q(name__icontains=token)
+    #     q_obj |= Q(description__icontains=token)
+    #
+    # return Shoes.objects.filter(q_obj)
