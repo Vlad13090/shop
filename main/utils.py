c73f68221158192f215454629b588129853e4707
Vlad <vl_mrgn@icloud.com>
2024-08-10 22:31:36 +0300

    Доделал полнотекстовый поиск

--- main/utils.py
+++ main/utils.py
@@ -1,10 +1,15 @@
 from django.db.models import Q
 
 from main.models import Shoes
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 
 
 def g_search(query):
-    return Shoes.objects.filter(description__search=query)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+    return Shoes.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
+
+    # return Shoes.objects.filter(description__search=query)
 
     # if len(query.split()) == 1:
     #     keywords = query.split()
