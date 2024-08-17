7762e0a3d581622decd81f14dd5eb8e157782941
Vlad <vl_mrgn@icloud.com>
2024-08-17 23:16:40 +0300

    Переделал CreateOrderView

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
 from django.db import transaction
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
 from carts.models import Cart
 from orders.forms import OrderForm
@@ -8,50 +11,90 @@ from .models import Order, OrderItem
 
 
 # Create your views here.
-def create_order(request):
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            try:
-                with transaction.atomic():
-                    user = request.user
-                    cart_items = Cart.objects.filter(user=user)
+# def create_order(request):
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             try:
+#                 with transaction.atomic():
+#                     user = request.user
+#                     cart_items = Cart.objects.filter(user=user)
+#                     if cart_items.exists():
+#                         order = Order.objects.create(
+#                             user=user,
+#                             number=form.cleaned_data['number'],
+#                             delivery=form.cleaned_data['delivery'],
+#                             address=form.cleaned_data['address'],
+#                             paid=form.cleaned_data['paid'],
+#                         )
+#                         for cart_item in cart_items:
+#                             product = cart_item.product
+#                             name = cart_item.product.name
+#                             price = cart_item.product.sell_price()
+#                             quantity = cart_item.quantity
+#
+#                             OrderItem.objects.create(
+#                                 order=order,
+#                                 product=product,
+#                                 name=name,
+#                                 price=price,
+#                                 quantity=quantity,
+#                             )
+#                         cart_items.delete()
+#
+#                         return redirect('user:profile')
+#             except ValidationError:
+#                 return redirect('user:profile')
+#     else:
+#         initial = {
+#             'first_name': request.user.first_name,
+#             'last_name': request.user.last_name,
+#         }
+#         form = OrderForm(initial=initial)
+#
+#     return render(request, 'orders/create_order.html', {'form': form})
 
-                    if cart_items.exists():
-                        # Создать заказ
-                        order = Order.objects.create(
-                            user=user,
-                            number=form.cleaned_data['number'],
-                            delivery=form.cleaned_data['delivery'],
-                            address=form.cleaned_data['address'],
-                            paid=form.cleaned_data['paid'],
+
+class CreateOrderView(LoginRequiredMixin, FormView):
+    template_name = 'orders/create_order.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('user:profile')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        return initial
+
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+                user = self.request.user
+                cart_items = Cart.objects.filter(user=user)
+
+                if cart_items.exists():
+                    order = Order.objects.create(
+                        user=user,
+                        number=form.cleaned_data['number'],
+                        delivery=form.cleaned_data['delivery'],
+                        address=form.cleaned_data['address'],
+                        paid=form.cleaned_data['paid'],
+                    )
+                    for cart_item in cart_items:
+                        product = cart_item.product
+                        name = cart_item.product.name
+                        price = cart_item.product.sell_price()
+                        quantity = cart_item.quantity
+
+                        OrderItem.objects.create(
+                            order=order,
+                            product=product,
+                            name=name,
+                            price=price,
+                            quantity=quantity,
                         )
-                        # Создать заказанные товары
-                        for cart_item in cart_items:
-                            product = cart_item.product
-                            name = cart_item.product.name
-                            price = cart_item.product.sell_price()
-                            quantity = cart_item.quantity
+                    cart_items.delete()
 
-                            OrderItem.objects.create(
-                                order=order,
-                                product=product,
-                                name=name,
-                                price=price,
-                                quantity=quantity,
-                            )
-
-                        # Очистить корзину после создания заказа
-                        cart_items.delete()
-
-                        return redirect('user:profile')
-            except ValidationError:
-                return redirect('user:profile')
-    else:
-        initial = {
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name,
-        }
-        form = OrderForm(initial=initial)
-
-    return render(request, 'orders/create_order.html', {'form': form})
+                    return redirect('user:profile')
+        except ValidationError:
+            return redirect('user:profile')
