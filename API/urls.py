9e579b69e3f3ea5fab96334c48356af1a4f0a31a
Vlad <vl_mrgn@icloud.com>
2024-08-21 15:20:45 +0300

    update API (DefaultRouter)

--- API/urls.py
+++ API/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
 app_name = 'API'
 
-router = SimpleRouter()
-router.register(r'shoes', views.ShoesViewSet)
+router = DefaultRouter()
+router.register(r'shoes', views.ShoesViewSet, basename='shoes')
 
 urlpatterns = [
     path('', include(router.urls)),  # http://localhost:8000/api/shoes/  or  http://localhost:8000/api/shoes/<int:pk>/
--- API/views.py
+++ API/views.py
@@ -1,8 +1,23 @@
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from API.serializers import ShoesSerializer
-from main.models import Shoes
+from main.models import Shoes, Category
 
 
 class ShoesViewSet(viewsets.ModelViewSet):
-    queryset = Shoes.objects.all()
+    # queryset = Shoes.objects.all()
     serializer_class = ShoesSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return Shoes.objects.all()[:3]
+        else:
+            return Shoes.objects.filter(pk=pk)
+
+    @action(methods=["GET"], detail=True)  # Новый роут http://localhost:8000/api/shoes/1/categories/
+    def categories(self, request, pk=None):
+        categories = Category.objects.get(pk=pk)
+        return Response({"categories": categories.name})
